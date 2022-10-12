042cdd72b98d41673e305df7274cc43816a7e7b3
honestyman <leel50296@gmail.com>
2022-10-12 09:54:24 +0200

    Initial commit

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,115 @@
+<?php
+
+  
+
+namespace App\Http\Controllers;
+
+  
+
+use Illuminate\Http\Request;
+
+use Laravel\Socialite\Facades\Socialite;
+
+use Exception;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+
+  
+
+class GoogleController extends Controller
+
+{
+
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return void
+
+     */
+
+    public function redirectToGoogle()
+
+    {
+
+        return Socialite::driver('google')->redirect();
+
+    }
+
+          
+
+    /**
+
+     * Create a new controller instance.
+
+     *
+
+     * @return void
+
+     */
+
+    public function handleGoogleCallback()
+
+    {
+
+        try {
+
+        
+
+            $user = Socialite::driver('google')->user();
+
+         
+
+            $finduser = User::where('google_id', $user->id)->first();
+
+         
+
+            if($finduser){
+
+         
+
+                Auth::login($finduser);
+
+        
+
+                return redirect()->intended('dashboard');
+
+         
+
+            }else{
+
+                $newUser = User::updateOrCreate(['email' => $user->email],[
+
+                        'name' => $user->name,
+
+                        'google_id'=> $user->id,
+
+                        'password' => encrypt('123456dummy')
+
+                    ]);
+
+         
+
+                Auth::login($newUser);
+
+        
+
+                return redirect()->intended('dashboard');
+
+            }
+
+        
+
+        } catch (Exception $e) {
+
+            dd($e->getMessage());
+
+        }
+
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+  
+
+use App\Http\Controllers\GoogleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::controller(GoogleController::class)->group(function(){
+
+    Route::get('auth/google', 'redirectToGoogle')->name('auth.google');
+
+    Route::get('auth/google/callback', 'handleGoogleCallback');
+
+});
