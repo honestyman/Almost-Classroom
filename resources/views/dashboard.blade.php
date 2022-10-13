f26024b96a200d34e487efba8ab023e0c06521c9
honestyman <leel50296@gmail.com>
2022-10-13 08:40:28 +0200

    Added fontawesome

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Všechny příspěvky
+            Všechny příspěvky 
         </h2>
+        <i class="fa-solid fa-gear"></i>
     </x-slot>
 
     <div class="py-12">
