86229916b16d8c08fda3ae33932a0776b17defdf
honestyman <leel50296@gmail.com>
2023-02-01 12:16:30 +0100

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Stepanek Classroom') }}
+                        {{ __('Domů') }}
                     </x-nav-link>
                 </div>
             </div>
