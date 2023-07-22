a6f721b2b2d86b29d54634d16f79e878dc2670e4
honestyman <leel50296@gmail.com>
2023-07-22 13:30:59 +0200

    Update 2022_10_04_191909_add_google_id_column.php

--- database/migrations/2022_10_04_191909_add_google_id_column.php
+++ database/migrations/2022_10_04_191909_add_google_id_column.php
@@ -1,59 +1,30 @@
 <?php
 
-  
-
 use Illuminate\Database\Migrations\Migration;
-
 use Illuminate\Database\Schema\Blueprint;
-
 use Illuminate\Support\Facades\Schema;
 
-  
-
 return new class extends Migration
-
 {
-
     /**
-
      * Run the migrations.
-
      *
-
      * @return void
-
      */
-
     public function up()
-
     {
-
         Schema::table('users', function ($table) {
-
             $table->string('google_id')->nullable();
-
         });
-
     }
-
-  
 
     /**
-
      * Reverse the migrations.
-
      *
-
      * @return void
-
      */
-
     public function down()
-
     {
-
-          
-
+        Schema::dropColumns('users', 'google_id');
     }
-
 };
